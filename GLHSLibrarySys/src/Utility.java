62a480116ca2db6990ce6c066b3ea742aee82667
GitOffMyPorch <54565884+GitOffMyPorch@users.noreply.github.com>
2020-03-22 22:02:19 -0600

    Add files via upload

--- GLHSLibrarySys/src/Utility.java
+++ GLHSLibrarySys/src/Utility.java
@@ -1,33 +1,28 @@
 import javax.swing.*;
 import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Utility {
 
+
     public static void usingBufferedWriter(ArrayList<LibraryBook> book) throws java.io.IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("data"));
-        for(int i = 0; i < book.size(); i++) {
-            String s1 = Integer.toString(book.get(i).returnBookNumber());
-            String s2 = s1.concat("_" + book.get(i).returnBookName());
-            String s3 = s2.concat("_" + book.get(i).returnAuthorName());
-            String s4 = s3.concat("_" + book.get(i).returnAbleToBeCheckedOut());
-            writer.write(s4 + "\n");
-        }
-        writer.close();
+                ArrayList<LibraryBook> object = book;
+                String filename = "data.ser";
+                FileOutputStream file = new FileOutputStream(filename);
+                ObjectOutputStream out = new ObjectOutputStream(file);
+                out.writeObject(object);
+                out.flush();
+                out.close();
     }
 
-    public static void usingReader() throws java.io.IOException {
-        Scanner scan = new Scanner(new File("data"));
-        while (scan.hasNextLine()) {
-            String str = scan.nextLine();
-            String[] arrayOfStr = str.split("_");
-            int number = Integer.valueOf(arrayOfStr[0]);
-            String name = arrayOfStr[1];
-            String author = arrayOfStr[2];
-            Boolean checked = Boolean.valueOf(arrayOfStr[3]);
-            LibraryFrontEnd.book.add(new LibraryBook(name, number, checked, author));
-        }
+    public static void usingReader() throws java.io.IOException, java.lang.ClassNotFoundException {
+                String filename = "data.ser";
+                FileInputStream file = new FileInputStream(filename);
+                ObjectInputStream in = new ObjectInputStream(file);
+                ArrayList<LibraryBook> object = (ArrayList<LibraryBook>) in.readObject();
+                LibraryFrontEnd.book = object;
+                in.close();
+                file.close();
     }
 
     public static void updateShowBooksTextArea(JTextArea showbooks) {
